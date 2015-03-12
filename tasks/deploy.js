c2da0bdeacc641001ac16533ffaf52b06a485e9e
Zeno Rocha <hi@zenorocha.com>
2015-03-12 17:25:55 -0300

    Add task to deploy to gh-pages branch - Fixes #2

---
+++ tasks/deploy.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var runSequence = require('run-sequence');
+
+gulp.task('gh-pages', function () {
+  return gulp.src('dist/public/**/*')
+    .pipe(plugins.ghPages());
+});
+
+gulp.task('deploy', ['clean'], function(cb) {
+  runSequence('build', 'gh-pages', cb);
+});

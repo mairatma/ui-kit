a080b25b61812ab5a536186c00f36f2c66257913
Zeno Rocha <hi@zenorocha.com>
2015-03-25 09:17:44 -0300

    Update 2015-03-25T12:17:39.777Z

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -1,4 +1,4 @@
-[].forEach.call(document.querySelectorAll('a'), function (elem) {
+[].forEach.call(document.querySelectorAll('.page-cell a'), function(elem) {
   elem.addEventListener('click', function(e) {
     e.preventDefault();
   });

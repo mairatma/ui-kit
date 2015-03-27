c8c8041e48bd5ef048bbc4b3ff5c1ea003445157
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-03-27 11:41:55 -0300

    Waits for the DOM to be loaded before instantiating a component to be able to capture scroll position when #hash is present

--- src/public/scripts/docs.js
+++ src/public/scripts/docs.js
@@ -1,15 +1,18 @@
-[].forEach.call(document.querySelectorAll('.docs-cell a'), function(elem) {
-  elem.addEventListener('click', function(e) {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", function(event) {
+  [].forEach.call(document.querySelectorAll('.docs-cell a'), function(elem) {
+    elem.addEventListener('click', function(e) {
+      e.preventDefault();
+    });
   });
-});
 
-new ui.Affix({
-  element: '#docs-sidebar',
-  offsetTop: 64
-});
+  new ui.Affix({
+    element: '#docs-sidebar',
+    offsetTop: 64
+  });
 
-new ui.Scrollspy({
-  element: '#docs-sidebar',
-  activeClass: 'sidebar-link-selected'
+  new ui.Scrollspy({
+    element: '#docs-sidebar',
+    offset: 100,
+    activeClass: 'sidebar-link-selected'
+  });
 });

f24c8ca9df3d09c1efe3f78843f452afae603344
Zeno Rocha <hi@zenorocha.com>
2015-04-09 23:05:34 -0700

    Improves release task

--- tasks/release.js
+++ tasks/release.js
@@ -1,13 +1,16 @@
 var gulp = require('gulp');
+var runSequence = require('run-sequence');
 
-gulp.task('release', ['release-styles', 'release-scripts']);
+gulp.task('release', function(cb) {
+  runSequence('build', ['release-styles', 'release-scripts'], cb);
+});
 
-gulp.task('release-styles', ['build-all'], function() {
-  gulp.src('dist/public/styles/main.css')
+gulp.task('release-styles', function() {
+  gulp.src('dist/public/styles/ui-kit.css')
     .pipe(gulp.dest('build/styles'));
 });
 
-gulp.task('release-scripts', ['build-all'], function() {
-  gulp.src('dist/public/scripts/*')
+gulp.task('release-scripts', function() {
+  gulp.src('dist/public/scripts/ui-kit.js')
     .pipe(gulp.dest('build/scripts'));
 });

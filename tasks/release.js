bbcfcd73eea346390ae42a11abfd4eda6a6961d3
Zeno Rocha <hi@zenorocha.com>
2015-04-13 15:10:17 -0700

    Adds fonts to release package

--- tasks/release.js
+++ tasks/release.js
@@ -2,7 +2,16 @@ var gulp = require('gulp');
 var runSequence = require('run-sequence');
 
 gulp.task('release', function(cb) {
-  runSequence('build', ['release-styles', 'release-scripts'], cb);
+  runSequence('build', ['release-fonts', 'release-styles', 'release-scripts'], cb);
+});
+
+gulp.task('release-fonts', function() {
+  gulp.src([
+    'dist/public/fonts/*.eot',
+    'dist/public/fonts/*.svg',
+    'dist/public/fonts/*.ttf',
+    'dist/public/fonts/*.woff'
+  ]).pipe(gulp.dest('build/fonts'));
 });
 
 gulp.task('release-styles', function() {

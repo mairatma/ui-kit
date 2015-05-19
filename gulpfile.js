f64048d670cc3093828019e827e3066f5cd853a5
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2015-05-19 11:47:57 +0200

    Adds TODO: Removes depencency on tasks project.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 'use strict';
 // Load liferay-sdk tasks
+// TODO: Removes depencency on tasks project.
 try { require('require-dir')('node_modules/liferay-sdk/tasks'); } catch (err) {}
 // Load custom tasks from the `tasks` directory
 try { require('require-dir')('tasks'); } catch (err) {}
